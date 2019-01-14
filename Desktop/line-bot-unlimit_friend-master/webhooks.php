495129aad3d0e1bfdf9f4021c9627f6829096dc9
modzaasard <46655620+modzaasard@users.noreply.github.com>
2019-01-14 18:05:04 +0700

    Update webhooks.php

--- Desktop/line-bot-unlimit_friend-master/webhooks.php
+++ Desktop/line-bot-unlimit_friend-master/webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'vJIEowPmdyfHgrc5O2TSSloPoXb6aiuyw1xQUBUxGImky2VMCHv/TYPOmdapEuvur85Sh6SSGYGumQxfk1vuSFd4lkYACEDzkI/aqKJQ9Vpu9QehboHvW7JsBGBgKAD1ecCbpLH0AliBEhFnHCY0NAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'Channel_Access_Token';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -20,37 +20,15 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text']."\n".$event['source']['userId']."\n";
 			// Get replyToken
 			$text2 = $text;
-			$replyToken = $event['replyToken'];
-			
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
 		}
 	}
 }
 require "vendor/autoload.php";
-$channelSecret = '0bc40aeffb71f2f8314581d2e01ebed5';
-$pushID = 'U0a4be35d402e2554c8388ed07897579e';
+$channelSecret = 'Channel_Secret';
+$pushID = 'Your_User_ID';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text2);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-echo "ok";
 echo "OK2";
