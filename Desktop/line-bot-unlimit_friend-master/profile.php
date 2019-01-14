543cb2dc1922c3960b99677cd88ccf647c076685
modzaasard <46655620+modzaasard@users.noreply.github.com>
2019-01-14 11:27:17 +0700

    update value

--- Desktop/line-bot-unlimit_friend-master/profile.php
+++ Desktop/line-bot-unlimit_friend-master/profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '*Channel_Access_Token*';
+$access_token = 'vJIEowPmdyfHgrc5O2TSSloPoXb6aiuyw1xQUBUxGImky2VMCHv/TYPOmdapEuvur85Sh6SSGYGumQxfk1vuSFd4lkYACEDzkI/aqKJQ9Vpu9QehboHvW7JsBGBgKAD1ecCbpLH0AliBEhFnHCY0NAdB04t89/1O/w1cDnyilFU=';
 
-$userId = '*User_ID*';
+$userId = 'U8a92bfd43082c7ff22087c34228c57cd';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
